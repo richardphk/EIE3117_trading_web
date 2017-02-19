832948e310c3300a4b42c2ef0d8fae9e3f8b9f87
yinktam0625 <yinktam0625@icloud.com>
2017-02-19 09:23:11 +0800

    Order page
    Improved user interface. Real-time calculator for price and quantity
    writen by javascript.

--- order/order_page.php
+++ order/order_page.php
@@ -8,76 +8,68 @@
 	$product_id = 'ABCD';
 
 	page_header('cart');
+	
 ?>
 	
-	<style type="text/css">
-    .Table
-    {
-        display: table;
-    }
-    .Title
-    {
-        display: table-caption;
-        text-align: center;
-        font-weight: bold;
-        font-size: larger;
-    }
-    .Heading
-    {
-        display: table-row;
-        font-weight: bold;
-        text-align: center;
-    }
-    .Row
-    {
-        display: table-row;
-    }
-    .Cell
-    {
-        display: table-cell;
-        border: solid;
-        border-width: thin;
-        padding-left: 5px;
-        padding-right: 5px;
-    }
-	</style>
 
-	<form action='order/order.php' method="post">
-		<div class="order_list">
-			<div class="Heading">
-				<div class="Cell">
-					<p>Item</p>
-				</div>
-				<div class="Cell">
-					<p>Price</p>
-				</div>
-				<div class="Cell">
-					<p>Quantity</p>
-				</div>
-				<div class="Cell">
-					<p>Total Price</p>
-				</div>
-			</div>
+	<script>
+		function calculator(price) {
+			var total_price;
+			var quantity = document.getElementById("quantity").value;
 			
-			<div class="Row">
-				<input name="product_id" type="hidden" value="<?php echo $product_id?>" />
-				<div class="Cell">
-					<p><?php echo '<img src="' . get_result($product_id, 'Image_Path') . '" height="100" width="100" />';?></p>
-					<p><?php echo get_result($product_id, 'Name'); ?></p>
-				</div>
-				<div class="Cell">
-					<p>$<?php echo get_result($product_id, 'Price'); ?></p>
-				</div>
-				<div class="Cell">
-					<select name="quantity" id="quantity"><?php echo get_result($product_id, 'Sale');
-												for ($i=0; $i<=get_result($product_id, 'Sale'); $i++) {
-													echo '<option value = "' . $i . '">' . $i . '</option>';
-												}
-												?></select>
-				</div>
-			</div>
-		</div>
-		<input type="submit" value="Purchase" />
+			total_price = price * quantity;
+			
+			document.getElementById("total_price").innerHTML="$" + total_price;
+		}
+	</script>
+	<link rel="stylesheet" href="https://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">  
+	<script src="https://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	<form id="order_list" action='order/order.php' method="post">
+		<input name="product_id" type="hidden" value="<?php echo $product_id?>" />
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>Item</th>
+					<th>Price</th>
+					<th>Quantity</th>
+					<th>Total Price</th>
+				</tr>
+			</thead>
+			
+			<tbody>
+				<tr>
+					<td>
+						<?php echo '<img src="' . get_result($product_id, 'Image_Path') . '" class="img-responsive" alt="Cinque Terre" />';?>
+						<?php echo get_result($product_id, 'Name'); ?>
+					</td>
+					<td id="price">
+						$<?php echo get_result($product_id, 'Price'); ?>
+					
+					</td>
+					<td>
+						<select name="quantity" id="quantity" onchange="calculator(<?php echo get_result($product_id, 'Price'); ?>)">
+							<?php echo get_result($product_id, 'Sale');
+								for ($i=0; $i<=get_result($product_id, 'Sale'); $i++) {
+									echo '<option value = "' . $i . '">' . $i . '</option>';
+								}
+							?>
+						</select>
+					</td>
+					<td>
+						<p id="total_price">$0</p>
+					</td>
+					
+					<tfoot>
+						<td rowspan="2">
+							<input type="submit" class="btn btn-primary btn-lg btn-block" value="Purchase" />
+						</td>
+					</tfoot>
+				</tr>
+			</tbody>
+			
+		</table>
 	</form>
 			
 
