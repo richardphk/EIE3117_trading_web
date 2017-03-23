2c0dd8b32039bf9f9640d0b915b369184f5cedf1
yinktam0625 <yinktam0625@icloud.com>
2017-03-23 21:52:38 +0800

    Corrected path problem

--- includes/gen_id.php
+++ includes/gen_id.php
@@ -3,7 +3,7 @@
 		//args = table name
 		//args : Tweb_User_ID->U, Tweb_Product_ID->P, Tweb_Sale_Record_ID->S, Tweb_Order_ID->O, Tweb_Payment_ID->A
 		//return false means error
-		require_once($_SERVER['DOCUMENT_ROOT'] . '/EIE3117_trading_web/config_db/config_db.php');
+		require_once($_SERVER['DOCUMENT_ROOT'] . '/config_db/config_db.php');
 		
 		// get db
 		$db = db_connect('root', '');
